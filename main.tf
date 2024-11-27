01d8996f6075e7877125a00033715395c194b920
Mina ayoub <minaayoub@gmail.com>
2024-11-27 09:27:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,9 @@ resource "azurerm_resource_group" "oidc" {
   name     = var.resource_group_name
   location = var.location
 }
+
+
+resource "azuread_access_package_catalog" "catalog1" {
+  display_name = "AzureADRoles_PIM"
+  description  = "This catalog holds Azure AD roles to be put in access packages"
+}
