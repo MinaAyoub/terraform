080afc807613f5a4a9c73631c7f987c1071b3018
MINA <MINA@email.com>
2024-11-27 13:17:26 -0500

    lots of changes

--- main.tf
+++ main.tf
@@ -3,7 +3,164 @@ provider "azurerm" {
   use_oidc = true
 }
 
-resource "azurerm_resource_group" "oidc" {
-  name     = var.resource_group_name
-  location = var.location
+provider "azuread" {
+}
+
+#Create the groups for PIM for groups based on the map variable keys
+resource "azuread_group" "pimgroups" {
+  for_each = var.role_map
+
+  display_name          = each.key
+  security_enabled      = true
+  assignable_to_role    = true
+}
+
+
+#Flatten data in variable to create all roles for PIM for groups
+locals {
+  flattened_map = flatten([
+    for k, v in var.role_map : [
+      for id in v : {
+        group_name = k
+        role_id    = id
+      }
+    ]
+  ])
+}
+
+#Create roles assignment for PIM for groups, will assign the roles to the group id based on key values in the map variable
+resource "azuread_directory_role_assignment" "pag_assignments" {
+  for_each = { for i in local.flattened_map : "${i.group_name}-${i.role_id}" => i }
+
+  role_id             = each.value.role_id
+  principal_object_id = azuread_group.pimgroups[each.value.group_name].object_id
+}
+
+
+#########################################################
+############ Roles, Groups and Access packages ##########
+#########################################################
+#This section creates the access packages which contain a group that's assigned to a single role
+
+##########################
+### Roles and Groups #####
+##########################
+
+#Create all roles as resources 
+resource "azuread_directory_role" "roles" {
+  count        = length(var.roles_names)
+  display_name = var.roles_names[count.index]
+}
+
+#Create the groups to be assigned the roles 
+resource "azuread_group" "groups" {
+  count                 = length(var.roles_names)
+  display_name          = "GRP_AdminRole_${var.roles_names[count.index]}"
+  security_enabled      = true
+  assignable_to_role    = true
+}
+
+#Create the role owner groups for each role to be used as approvers for access packages 
+resource "azuread_group" "role_owners" {
+  count                 = length(var.roles_names)
+  display_name          = "GRP_RoleOwners_${var.roles_names[count.index]}"
+  security_enabled      = true
+  assignable_to_role    = true
+}
+
+#Create the dynamic admin group, this group will contain all admins, and ONLY they will be able to view and request access packages
+resource "azuread_group" "admin_group" {
+  display_name     = "GRP_UAT_Admins_AccessPackageRequest"
+  security_enabled = true
+  types            = ["DynamicMembership"]
+
+  dynamic_membership {
+    enabled = true
+    rule    = "user.userPrincipalName -contains \".ads\""
+  }
+}
+
+#Create eligible role assignments for groups in access packages
+resource "azuread_directory_role_eligibility_schedule_request" "elassign" {
+  count                 = length(var.roles_names)
+ 
+  role_definition_id = (azuread_directory_role.roles[count.index]).template_id
+  principal_id       = (azuread_group.groups[count.index]).id
+  directory_scope_id = "/"
+  justification      = "Given through access package"
+}
+
+###################################
+### Identity Governance Portion ###
+###################################
+
+#Create the catalog 
+resource "azuread_access_package_catalog" "catalog1" {
+  display_name = "CL_EntraIDRoles_PIM"
+  description  = "This catalog holds Azure AD roles to be put in access packages"
+}
+
+#Create the catalog resource association with the groups 
+resource "azuread_access_package_resource_catalog_association" "catalogassoc" {
+  count                  = length(var.roles_names)
+  catalog_id             = azuread_access_package_catalog.catalog1.id
+  resource_origin_id     = (azuread_group.groups[count.index]).id
+  resource_origin_system = "AadGroup"
+}
+
+#Create the access packages
+resource "azuread_access_package" "accesspackages" {
+  count        = length(var.roles_names)
+  catalog_id   = azuread_access_package_catalog.catalog1.id
+  display_name = "AP_EntraID_${var.roles_names[count.index]}"
+  description  = "Access package for ${var.roles_names[count.index]}"
+}
+
+#Create the access package resource association
+resource "azuread_access_package_resource_package_association" "apassoc" {
+  count                           = length(var.roles_names)
+  access_package_id               = (azuread_access_package.accesspackages[count.index]).id
+  catalog_resource_association_id = (azuread_access_package_resource_catalog_association.catalogassoc[count.index]).id
+}
+
+#Create the policy inside the access package
+resource "azuread_access_package_assignment_policy" "policy1" {
+  count             = length(var.roles_names)
+  access_package_id = (azuread_access_package.accesspackages[count.index]).id
+  display_name      = "${var.roles_names[count.index]}-policy"
+  description       = "Policy for ${var.roles_names[count.index]} access package"
+  duration_in_days  = 180
+
+  requestor_settings {
+    requests_accepted = true
+    scope_type = "SpecificDirectorySubjects"
+
+    requestor {
+      object_id = azuread_group.admin_group.object_id
+      subject_type = "groupMembers"
+    }
+
+  }
+
+  approval_settings {
+    approval_required = true
+    requestor_justification_required = true 
+  
+    approval_stage {
+      approval_timeout_in_days = 14
+
+      primary_approver {
+        object_id    = azuread_group.role_owners[count.index].object_id
+        subject_type = "groupMembers"
+      }
+    }
+  }
+
+  assignment_review_settings {
+    enabled                        = true
+    review_frequency               = "quarterly"
+    duration_in_days               = 3
+    review_type                    = "Self"
+    access_review_timeout_behavior = "removeAccess"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,55 @@
-variable "resource_group_name" {
-  type        = string
-  description = "(Optional) Name of resource group to create. Defaults to oidc-test."
-  default     = "oidc-simple-04062023"
+
+#This variable holds the values for the PIM for groups, the group names and the roles to be added 
+variable "role_map" {
+  type = map(list(string))
+  default = {
+    "RLGRP_UAT_IdentityAdmins" = ["fe930be7-5e62-47db-91af-98c3a49a38b1", 
+                                  "b1be1c3e-b65d-4f19-8427-f6fa0d97feb9", 
+                                  "7be44c8a-adaf-4e2a-84d6-ab2649e08a13", 
+                                  "fdd7a751-b60b-444a-984c-02652fe8fa1c", 
+                                  "fdd7a751-b60b-444a-984c-02652fe8fa1c", 
+                                  "c4e39bd9-1100-46d3-8c65-fb160da0071f", 
+                                  "7be44c8a-adaf-4e2a-84d6-ab2649e08a13", 
+                                  "9360feb5-f418-4baa-8175-e2a00bac4301",
+                                  "3edaf663-341e-4475-9f94-5c398ef6c070", 
+                                  "3edaf663-341e-4475-9f94-5c398ef6c070", 
+                                  "0526716b-113d-4c15-b2c8-68e3c22b9f80", 
+                                  "5b784334-f94b-471a-a387-e7219fc49ca2"]
+    "RLGRP_UAT_InfraAdmins" = ["fe930be7-5e62-47db-91af-98c3a49a38b1", 
+                                "17315797-102d-40b4-93e0-432062caca18", 
+                                "9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3", 
+                                "fe930be7-5e62-47db-91af-98c3a49a38b1", 
+                                "158c047a-c907-4556-b7ef-446551a6b5f7", 
+                                "b1be1c3e-b65d-4f19-8427-f6fa0d97feb9", 
+                                "b0f54661-2d74-4c50-afa3-1ec803f12efe", 
+                                "fdd7a751-b60b-444a-984c-02652fe8fa1c", 
+                                "e3973bdf-4987-49ae-837a-ba8e231c7286", 
+                                "8329153b-31d0-4727-b945-745eb3bc5f31", 
+                                "892c5842-a9a6-463a-8041-72aa08ca3cf6"]
+    "RLGRP_UAT_SecurityAdmins" = ["fe930be7-5e62-47db-91af-98c3a49a38b1", 
+                                  "158c047a-c907-4556-b7ef-446551a6b5f7", 
+                                  "b1be1c3e-b65d-4f19-8427-f6fa0d97feb9", 
+                                  "fdd7a751-b60b-444a-984c-02652fe8fa1c", 
+                                  "3d6c7d3e-0c4e-4d3e-8b7b-1a8b1b1b1b1b"]
+     }
 }
 
-variable "location" {
-  type        = string
-  description = "(Optional) Azure region to use. Defaults to East US."
-  default     = "eastus"
+#Variables to hold all the names to be used to generate everything else, adding a new value here will create a new group and access package etc
+variable "roles_names" {
+  type    = list(string)
+  default = [
+              "Application Administrator", 
+              "Application Developer", 
+              "Billing Administrator", 
+              "Global Administrator", 
+              "Global Reader", 
+              "Groups Administrator", 
+              "Security Administrator", 
+              "Security Operator", 
+              "Security Reader", 
+              "User Administrator"
+]
 }
+
+
+
