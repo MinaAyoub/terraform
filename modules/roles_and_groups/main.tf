c1b4adbe23d4dcaf980aee1f951b9ae957970eaf
MINA <mina.ayoub@avanade.com>
2025-11-24 15:42:30 -0500

    tenant id

--- modules/roles_and_groups/main.tf
+++ modules/roles_and_groups/main.tf
@@ -25,7 +25,7 @@ data "azurerm_role_definition" "roles" {
   for_each = toset(var.roles_names)
   name     = each.key
   #scope    = "/subscriptions/${var.subscription_id}"
-  scope    = "/${var.tenant_id}"
+  scope    = "${var.tenant_id}"
 }
 
 resource "time_static" "start" {}
@@ -34,7 +34,7 @@ resource "time_static" "start" {}
 resource "azurerm_pim_eligible_role_assignment" "example" {
   for_each            = data.azurerm_role_definition.roles
   #scope               = "/subscriptions/${var.subscription_id}"
-  scope                = "/${var.tenant_id}"
+  scope                = "${var.tenant_id}"
   role_definition_id  = each.value.id
   principal_id        = azuread_group.groups[each.key].object_id
   schedule {
